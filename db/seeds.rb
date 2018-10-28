adbb541e20dd9d2829064bb431b870ffb3c3dc38
Viki Harrod <victoria.harrod@gmail.com>
2018-10-28 15:40:31 -0600

    Simplify seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
 # Creating non-shipped orders.
-o1 = Order.create!()
-o2 = Order.create!()
-o3 = Order.create!()
-o4 = Order.create!()
-o5 = Order.create!()
-# Shipped at times are randomly assigned.  What great randomizaiton I've done!
-os1 = Order.create!(shipped_at: Time.now + 21.minute)
-os2 = Order.create!(shipped_at: Time.now + 20.minute)
-os3 = Order.create!(shipped_at: Time.now + 3.minute)
-os4 = Order.create!(shipped_at: Time.now + 40.minute)
-os5 = Order.create!(shipped_at: Time.now + 5.minute)
+puts "Creating non-shipped orders."
+5.times do
+  Order.create!
+end
+
+puts "Creating shipped orders."
+5.times do
+  Order.create!(shipped_at: Time.now + rand(60).minute)
+end
